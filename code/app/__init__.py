3e0b0f7209cf83b6b7d5bb8c2147c3ba7bf2b11e
PHMark <canedam2016@gmail.com>
2020-08-05 18:27:57 +0800

    flask database migrate

--- code/app/__init__.py
+++ code/app/__init__.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from flask_restful import Api
+from flask_migrate import Migrate
 
 from .api.resources.seat_reservation import (
     SeatReservationResource,
@@ -13,9 +14,8 @@ from .api.resources.account import (
     AccountLogoutResource,
 )
 from .api.resources.user import UserRegisterResource, UserResource
-from .api.resources.cinema import CinemaResource
+from .api.resources.cinema import CinemaResource, CinemaUserResourse
 from .authenticate import jwt
-from .serializer import ma
 from .routes import (
     SEAT_RESERVATION_LIST_ROUTES,
     SEAT_RESERVATION_ROUTES,
@@ -24,13 +24,15 @@ from .routes import (
     ACCOUNT_ROUTES,
     ACCOUNT_LOGIN_ROUTES,
     ACCOUNT_LOGOUT_ROUTES,
-    CINEMA_ROUTES
+    CINEMA_ROUTES,
+    CINEMA_USER_ROUTES
 )
 from db import db
 from app.config import config
 
 
 api = Api()
+migrate = Migrate()
 
 
 def create_app(*, config_name="default") -> Flask:
@@ -46,9 +48,11 @@ def create_app(*, config_name="default") -> Flask:
     api.add_resource(AccountLogoutResource, *ACCOUNT_LOGOUT_ROUTES)
     api.add_resource(AccountResource, *ACCOUNT_ROUTES)
     api.add_resource(CinemaResource, *CINEMA_ROUTES)
+    api.add_resource(CinemaUserResourse, *CINEMA_USER_ROUTES)
+
     config[config_name].init_app(app=app)
     api.init_app(app=app)
     jwt.init_app(app=app)
     db.init_app(app=app)
-    ma.init_app(app=app)
+    migrate.init_app(app=app, db=db)
     return app
